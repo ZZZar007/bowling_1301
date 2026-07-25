e09b242a78059fa22729926ea01fd7d3b61d4eca
Naris222 <sing.num42@gmail.com>
2026-07-25 11:03:29 +0700

    save1

--- Assets/scripts/Bowling.cs
+++ Assets/scripts/Bowling.cs
@@ -1,16 +1,45 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Bowling : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField]
+    private Rigidbody rb;
+
+    [SerializeField]
+    private int forcePower;
+        void Start()
     {
-        
+        rb= GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Keyboard.current.spaceKey.wasPressedThisFrame)
+            ShootBall();
+
+        if (Keyboard.current.rightArrowKey.isPressed
+            || Keyboard.current.dKey.isPressed)
+            MoveRight();
+
+        if (Keyboard.current.leftArrowKey.isPressed
+            || Keyboard.current.aKey.isPressed)
+            MoveLeft();
+    }
+
+    private void ShootBall()
+    {
+        rb.AddForce(Vector3.forward * forcePower, ForceMode.Impulse);
+    }
+
+    private void MoveRight()
+    {
+        transform.position += new Vector3(0.5f, 0f, 0f)*Time.deltaTime;
+    }
+
+    private void MoveLeft()
+    {
+        transform.position += new Vector3(-0.5f, 0f, 0f) * Time.deltaTime;
     }
 }
